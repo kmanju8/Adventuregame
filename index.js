9eef9db5cee4d8bfa26a613797b2abf9905c902f
kmanju8 <keshava249@gmail.com>
2021-03-12 19:30:00 +0000

    fix async issues

--- index.js
+++ index.js
@@ -59,44 +59,39 @@ function startGame() {
   let damage=0;
   let correct = false;
   //choice will be user choice of difficulty, to change damage output
-  fetch("https://opentdb.com/api.php?amount=2&difficulty=".concat(temp,"&encode=url3986"))
-  .then(request=>request.json()).then(function(data){
+  let questiondat;
+  await fetch("https://opentdb.com/api.php?amount=2&difficulty=".concat(temp,"&encode=url3986")).then(response => {return response.json();}).then(data =>  { questiondat = data;}).catch(console.log(""));
   
-    const difficulty = data.results[0].difficulty;
-    const answers = data.results[0].incorrect_answers;
-    answers.push(data.results[0].correct_answer);
-    shuffleArray(answers);
-    console.log(decodeURIComponent(data.results[0].question));
-    for(const i in answers){
-        console.log((parseInt(i)+1),")",decodeURIComponent(answers[i]));
-    }
+  const difficulty = questiondat.results[0].difficulty;
+  const answers = questiondat.results[0].incorrect_answers;
+  answers.push(questiondat.results[0].correct_answer);
+  shuffleArray(answers);
+  console.log(decodeURIComponent(questiondat.results[0].question));
+  for(const i in answers){
+      console.log((parseInt(i)+1),")",decodeURIComponent(answers[i]));
+  }
     
-    let ans = prompt()
-    if(answers[parseInt(ans)-1]===data.results[0].correct_answer){
-        switch(difficulty){
-            case "easy":
-                damage = 3;
-                break;
-            case "medium":
-                damage = 5;
-                break;
-            case "hard":
-                damage = 7;
-                break;
+  let ans = prompt()
+  if(answers[parseInt(ans)-1]===questiondat.results[0].correct_answer){
+      switch(difficulty){
+          case "easy":
+              damage = 3;
+              break;
+          case "medium":
+              damage = 5;
+              break;
+          case "hard":
+              damage = 7;
+              break;
   
-        }
-        correct = true;
-        console.log("Correct!")
-    } else{
-        console.log("Incorrect! You take 2 damage.")
-        damage = -2;
-    }
-  }).catch(function () {
-    console.log("Whoops, there was an error in the program. Have a free 2 damage on us ;)");
-    damage = 2;
-  });
-  //issue is that whole fetch sequence counts as asynch function. Is added to buffer, the multichoicebat func finishes and everything goes bad.
-  
+      }
+      correct = true;
+      console.log("Correct!")
+  } else{
+      console.log("Incorrect! You take 2 damage.")
+      damage = -2;
+  }
+  handleAnswer("continue");
   }
 
   // story function 
